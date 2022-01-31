58fa533ca96ca61d080b0780cdd75fe61a9da1d7
kg0r0 <kg0r0@yahoo.co.jp>
2022-02-01 00:08:06 +0900

    Amazon EC2 instance using AWS CDK

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+## References
+- https://aws.amazon.com/jp/getting-started/guides/deploy-webapp-ec2/
+- https://docs.aws.amazon.com/cdk/api/v2/docs/aws-construct-library.html
+- https://github.com/aws-samples/aws-cdk-examples/tree/master/typescript/ec2-instance
--- lib/ec2-cdk-stack.ts
+++ lib/ec2-cdk-stack.ts
@@ -1,16 +1,98 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { KeyPair } from 'cdk-ec2-key-pair';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class Ec2CdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class Ec2CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Ec2CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Look up the default VPC
+    const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
+      isDefault: true
+    });
+
+    // Create a key pair to be used with this EC2 Instance
+    const key = new KeyPair(this, 'KeyPair', {
+      name: 'cdk-keypair',
+      description: 'Key Pair created with CDK Deployment',
+    });
+    key.grantReadOnPublicKey;
+
+
+    // Security group for the EC2 instance
+    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'Allow SSH (TCP port 22) and HTTP (TCP port 80) in ',
+      allowAllOutbound: true,
+    });
+
+    // Allow SSH access on port tcp/22
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allow SSH Access'
+    );
+
+    // Allow HTTP access on port tcp/80
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'Allow HTTP Access'
+    );
+    // Import an existing security group 
+    // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2.SecurityGroup.html
+    // const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(this, 'SG', 'sg-12345', {});
+
+    // IAM role to allow access to other AWS services
+    const role = new iam.Role(this, 'ec2Role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+
+    // IAM policy attachment to allow access to
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
+    );
+
+    // Look up the AMI Id for the Amazon Linux 2 Image with CPU Type X86_64
+    const ami = new ec2.AmazonLinuxImage({
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      cpuType: ec2.AmazonLinuxCpuType.X86_64,
+    });
+
+    // Create the EC2 instance using the Security Group, AMI, and KeyPair defined.
+    const ec2Instance = new ec2.Instance(this, 'Instance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ami,
+      securityGroup: securityGroup,
+      keyName: key.keyPairName,
+      role: role,
+    });
+
+    // Create outputs for connecting
+
+    // Output the public IP address of the EC2 instance
+    new cdk.CfnOutput(this, 'IP Address', {
+      value: ec2Instance.instancePublicIp,
+    });
+
+    // Command to download the SSH key
+    new cdk.CfnOutput(this, 'Download Key Command', {
+      value: 'aws secretsmanager get-secret-value --secret-id ec2-ssh-key/cdk-keypair/private --query SecretString --output text > cdk-key.pem && chmod 400 cdk-key.pem',
+    })
+
+    // Command to access the EC2 instance using SSH
+    new cdk.CfnOutput(this, 'ssh command', {
+      value:
+        'ssh -i cdk-key.pem -o IdentitiesOnly=yes ec2-user@' +
+        ec2Instance.instancePublicIp,
+    });
   }
 }
